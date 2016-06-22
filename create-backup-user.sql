03a009c7a4f1b13f840ee6a3fab974fd9d2d0588
David Egan <david@carawebs.com>
2016-06-22 22:05:41 +0100

    Provide instructions for creating a backup MySQL user. This is a once-off operation.

---
+++ create-backup-user.sql
@@ -0,0 +1,13 @@
+# Create a MySQL user with limited privileges for backup purposes.
+# This isn't strictly necessary on a local machine - you could just use the root
+# MySQL user in the backup script.
+# ==============================================================================
+
+# Get a MySQL/MariaDB prompt:
+mysql -u root -p
+
+# In the MySQL shell enter:
+CREATE USER 'backup_user'@'localhost' IDENTIFIED BY 'CiCHVQ2o8TSCq31SbWhQBkmn6';
+GRANT SELECT, SHOW VIEW, RELOAD, REPLICATION CLIENT, EVENT, TRIGGER ON *.* TO 'backup_user'@'localhost';
+FLUSH PRIVILEGES;
+SHOW GRANTS FOR 'backup_user'@'localhost';
